08257af19361f36e3105e20e9438fa90a0eca880
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2021-12-19 18:27:57 -0300

    Process testelapse column

--- script.py
+++ script.py
@@ -10,22 +10,24 @@ df = pd.DataFrame(data)
 
 # Data preparation
 
-
-# Convert time lapses to numeric
+# Process introelapse column (convert to numeric, replace missing with mean, discretize)
 df["introelapse"] = pd.to_numeric(df["introelapse"])
 
 df["introelapse"] = df["introelapse"].fillna(df["introelapse"].mean())
 
-
-
 df["introelapse"] = pd.cut(df["introelapse"],
-       bins=[0, 25, 50, 75, 100,99999999999],
+       bins=[0, 25, 50, 75, 100, np.Inf],
        labels=["Very Fast", "Fast", "Average", "Slow", "Very Slow"])
 
+# Process testelapse column (convert to numeric, replace missing with mean, discretize)
 
+df["testelapse"] = pd.to_numeric(df["testelapse"])
 
+df["testelapse"] = df["testelapse"].fillna(df["testelapse"].mean())
 
-
+df["testelapse"] = pd.cut(df["testelapse"],
+       bins=[0, 50, 100, 150, 200, np.Inf],
+       labels=["Very Fast", "Fast", "Average", "Slow", "Very Slow"])
 
 
 df.to_csv('processed_data.csv', index=False)
