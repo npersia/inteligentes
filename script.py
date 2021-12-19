cad75128cf9995a4f96d2d6851dc8204208cf1d6
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2021-12-19 17:42:19 -0300

    Discretize time lapse column example

--- script.py
+++ script.py
@@ -2,4 +2,16 @@ import pandas as pd
 
 data = pd.read_csv("data.csv", sep=',', error_bad_lines=False, index_col=False, dtype='unicode') 
 
+df = pd.DataFrame(data)
+
+# Data preparation
+
+df["introelapse"] = pd.to_numeric(df["introelapse"])
+
+df["introelapse"] = pd.cut(df["introelapse"],
+       bins=[0, 25, 50, 75, 100, 125], 
+       labels=["Muy rápido", "Rápido", "Promedio", "Lento", "Muy lento"])
+
+print(df["introelapse"])
+
 print(data.head())
