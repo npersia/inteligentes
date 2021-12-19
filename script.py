ade2104b89fb6aced7b5f97db5a57f97c09ef136
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2021-12-19 17:46:10 -0300

    Generate process data csv

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import numpy as np
 
 data = pd.read_csv("data.csv", sep=',', error_bad_lines=False, index_col=False, dtype='unicode') 
 
@@ -12,6 +13,6 @@ df["introelapse"] = pd.cut(df["introelapse"],
        bins=[0, 25, 50, 75, 100, 125], 
        labels=["Muy rápido", "Rápido", "Promedio", "Lento", "Muy lento"])
 
-print(df["introelapse"])
+# print(df["introelapse"])
 
-print(data.head())
+df.to_csv('processed_data.csv')
