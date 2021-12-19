95c2cc99d35bd8e956dbe7d26d37d96a85d1b68a
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2021-12-19 17:55:36 -0300

    Remove malformed line

--- script.py
+++ script.py
@@ -7,12 +7,15 @@ df = pd.DataFrame(data)
 
 # Data preparation
 
+# Remove malformed lines (#13507)
+
+# Convert time lapses to numeric
 df["introelapse"] = pd.to_numeric(df["introelapse"])
 
 df["introelapse"] = pd.cut(df["introelapse"],
        bins=[0, 25, 50, 75, 100, 125], 
        labels=["Muy rápido", "Rápido", "Promedio", "Lento", "Muy lento"])
 
-# print(df["introelapse"])
+
 
 df.to_csv('processed_data.csv', index=False)
