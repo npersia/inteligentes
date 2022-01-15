d60b83b47e19bb2e2244c88dc7030557fbc1538b
nahuel <persianahuel@gmail.com>
2022-01-15 12:07:01 -0300

    solar: add data.csv

--- solar/script.py
+++ solar/script.py
@@ -14,14 +14,14 @@ df.drop('Time', axis=1, inplace=True)
 
 df['UNIXTime'] = (pd.to_datetime(df['UNIXTime'],unit='s') + timedelta(hours = -10))
 
-__Date = []
+#__Date = []
 __time = []
 __sunrise = []
 __sunset = []
 for i in range(len(df["UNIXTime"])):
     date = df["UNIXTime"][i]
-    __Date.append(str(date.day).rjust(2, '0')+'/'+str(date.month).rjust(2, '0'))
-    __time.append(str(date.hour).rjust(2, '0')+':'+str(date.minute).rjust(2, '0'))
+    #__Date.append(str(date.day).rjust(2, '0')+'/'+str(date.month).rjust(2, '0'))
+    __time.append(str(date.hour).rjust(2, '0'))
 
     __sunrise.append(str(df["TimeSunRise"][i].split(":")[1]).rjust(2, '0'))
     __sunset.append(str(df["TimeSunSet"][i].split(":")[1]).rjust(2, '0'))
@@ -29,38 +29,92 @@ for i in range(len(df["UNIXTime"])):
 
     df["Pressure"][i] = str(df["Pressure"][i].split(".")[1]).ljust(2, '0')
 
-
-df['Date'] = __Date
+#df['Date'] = __Date
 df['Hour'] = __time
 df['SunRise'] = __sunrise
 df['SunSet'] = __sunset
 
 
+df["Hour2"] = pd.to_numeric(df["Hour"])
+df["Hour2"] = pd.cut(df["Hour2"],
+       bins=[0,5,11,14,20,24],
+       labels=["Night","Morning","Midday","Afternoon","Night"],
+                    ordered=False)
+
+
+df["SunRise"] = pd.to_numeric(df["SunRise"])
+df["SunRise"] = pd.cut(df["SunRise"],
+       bins=[0,20,40,60],
+       labels=["0-20","20-40","40-60"])
+
+df["SunSet"] = pd.to_numeric(df["SunSet"])
+df["SunSet"] = pd.cut(df["SunSet"],
+       bins=[0,20,40,60],
+       labels=["0-20","20-40","40-60"])
+
+
+df["UNIXTime"] = pd.to_numeric(df["UNIXTime"])
+df["fortnight"] = pd.cut(df["UNIXTime"],
+       bins=[1472698800000000000, 1473994800000000000, 1475290800000000000, 1476586800000000000, 1477969200000000000, 1479265200000000000, 1480561200000000000, 1481857200000000000, 1483239600000000000],
+       labels=["fortnight 1","fortnight 2", "fortnight 3", "fortnight 4", "fortnight 5", "fortnight 6", "fortnight 7", "fortnight 8"])
+
+df["month"] = pd.cut(df["UNIXTime"],
+       bins=[1472698800000000000 , 1475290800000000000 , 1477969200000000000 , 1480561200000000000, 1483239600000000000],
+       labels=["Sep", "Oct", "Nov", "Dec"])
+
+df["WindDirection(Degrees)"] = pd.to_numeric(df["WindDirection(Degrees)"])
+df["WindDirection(Degrees)"] = pd.cut(df["WindDirection(Degrees)"],
+       bins=[0,45,135,225,315,360],
+       labels=["N","E","S","W","N"],
+       ordered=False)
+
+
+"""df["Radiation"] = pd.to_numeric(df["Radiation"])
+df["Radiation"] = pd.cut(df["Radiation"],
+       bins=[0, 100,300,500,700,900,  5000],
+       labels=["0-100","100-300", "300-500", "500-700", "700-900", "+900"])"""
 
 df["Radiation"] = pd.to_numeric(df["Radiation"])
 df["Radiation"] = pd.cut(df["Radiation"],
-       bins=[0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
-       labels=["0","100", "200", "300", "400", "500", "600", "700", "800", "900"])
+       bins=[0,300,700,  5000],
+       labels=["Low","Med","High"])
+
+
+
+"""df["Temperature"] = pd.to_numeric(df["Temperature"])
+df["Temperature"] = pd.cut(df["Temperature"],
+       bins=[0, 2.5, 5, 7.5, 10, 12.5, 15, 17.5, 20, 50],
+       labels=["0", "2.5", "5", "7.5", "10", "12.5", "15", "17.5", "20"])"""
+
 
 df["Temperature"] = pd.to_numeric(df["Temperature"])
 df["Temperature"] = pd.cut(df["Temperature"],
-       bins=[0, 2.5, 5, 7.5, 10, 12.5, 15, 17.5, 20, 50],
-       labels=["0", "2.5", "5", "7.5", "10", "12.5", "15", "17.5", "20"])
+       bins=[0, 15, 20, 50],
+       labels=["Cold","Normal","Heat"])
 
 
 df["Pressure"] = pd.to_numeric(df["Pressure"])
 df["Pressure"] = pd.cut(df["Pressure"],
-       bins=[0, 20, 25, 30, 35, 40, 45, 50, 55, 60],
-       labels=["0", "20", "25", "30", "35", "40", "45", "50", "55"])
+       bins=[0, 30, 50, 60],
+       labels=["Low", "Normal", "High"])
 
 
 df["Humidity"] = pd.to_numeric(df["Humidity"])
 df["Humidity"] = pd.cut(df["Humidity"],
-       bins=[0, 20, 40, 60, 80, 200],
-       labels=["0", "20", "40", "60", "80"])
+       bins=[0, 40, 70, 200],
+       labels=["Low", "Normal", "High"])
 
 df["Speed"] = pd.to_numeric(df["Speed"])
 df["Speed"] = pd.cut(df["Speed"],
-       bins=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 100],
-       labels=["0", "5", "10", "15", "20", "25", "30", "35", "40", "45"])
+       bins=[0, 20, 40, 100],
+       labels=["Low", "Normal", "High"])
+
+
+df['Rad'] = df['Radiation']
+#df = df.drop(df[df.Rad == "0-100"].index)
+df.drop('UNIXTime', axis=1, inplace=True)
+df.drop('TimeSunRise', axis=1, inplace=True)
+df.drop('TimeSunSet', axis=1, inplace=True)
+df.drop('Radiation', axis=1, inplace=True)
+
 df.to_csv('processed_data.csv', index=False)
